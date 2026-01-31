ec548c09d9c2e9905b2a05351d52b117415a03d7
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-01-31 17:15:02 +0700

    Source code for check camera devices

---
+++ src/cam-check/include/cam_check.h
@@ -0,0 +1,4 @@
+#ifndef CAM_CHECK_H
+#define CAM_CHECK_H
+
+#endif
---
+++ src/cam-check/src/cam_check.c
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#include "cam_check.h"
+
+int main()
+{
+    // Camera /dev/video0
+    cv::VideoCapture cap(0);
+
+    if (!cap.isOpened())
+    {
+        std::cerr << "ERROR: Cannot open camera" << std::endl;
+        return -1;
+    }
+
+    cv::Mat frame;
+
+    while (true)
+    {
+        cap >> frame;
+        if (frame.empty())
+        {
+            std::cerr << "ERROR: Empty frame" << std::endl;
+            break;
+        }
+
+        cv::imshow("Camera Check", frame);
+
+        // ESC key to exit
+        if (cv::waitKey(30) == 27)
+            break;
+    }
+
+    return 0;
+}
