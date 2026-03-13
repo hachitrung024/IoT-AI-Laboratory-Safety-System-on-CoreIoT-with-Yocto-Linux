dc208f8f92a96997358633a65216c516857ddb42
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-03-13 20:35:14 +0700

    Add arg for fps test limit

--- src/lsmy-python-lib/lsmy_python_lib/image_analytics_engine.py
+++ src/lsmy-python-lib/lsmy_python_lib/image_analytics_engine.py
@@ -2,6 +2,7 @@ import os
 import time
 import psutil
 import logging
+import argparse
 import threading
 import subprocess
 import numpy as np
@@ -448,8 +449,8 @@ class ImageAnalyticsEngine:
                                 style="fill:none;stroke:lime;stroke-width:3" />
                                 <rect x="5" y="5" width="160" height="65" rx="5" fill="black" fill-opacity="0.5" />
                                 <text x="15" y="25" font-family="monospace" font-size="14" fill="white">FPS: {self.pipeline_fps}</text>
-                                <text x="15" y="45" font-family="monospace" font-size="14" fill="white">AI Latency: {self.avg_inference_time}</text>
-                                <text x="15" y="65" font-family="monospace" font-size="14" fill="white">Pipeline Latency: {self.pipeline_latency}</text>
+                                <text x="15" y="45" font-family="monospace" font-size="14" fill="white">AI Latency: {self.avg_inference_time:.2f} ms</text>
+                                <text x="15" y="65" font-family="monospace" font-size="14" fill="white">Pipeline Latency: {self.avg_pipeline_latency:.2f} ms</text>
                                 {svg_content}
                             </svg>
                             """
@@ -538,9 +539,20 @@ def decode_blazeface(raw_data, score_threshold=0.75, width=640, height=480):
     return x, y, w, h, landmarks
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Image Analytics Engine")
+
+    parser.add_argument(
+        "--fps",
+        type=int,
+        default=15,
+        help="Camera FPS (default: 15)"
+    )
+
+    args = parser.parse_args()
+
     model_path = "/usr/share/models/blaze_face_short_range.tflite"
 
-    engine = ImageAnalyticsEngine(width=640, height=480, fps=15,
+    engine = ImageAnalyticsEngine(width=640, height=480, fps=args.fps,
                                model_path=model_path,
                                use_model=True, debug_mode=True)
     try:
