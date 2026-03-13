9713846040f2a4cc77ad8a4c9dfdbd10583b6941
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-03-13 08:35:14 +0700

    Fix: Decode blaze model

--- src/lsmy-python-lib/lsmy_python_lib/image_analytics_engine.py
+++ src/lsmy-python-lib/lsmy_python_lib/image_analytics_engine.py
@@ -161,6 +161,7 @@ class ImageAnalyticsEngine:
                     f"tensor_converter ! "
                     f"tensor_transform mode=arithmetic option=typecast:float32,div:255.0 ! "
                     f"tensor_filter framework=tensorflow2-lite model={self.model_path} ! "
+                    f"tensor_decoder mode=bounding_boxes option1=blazeface ! "
                     f"appsink name=appsink emit-signals=true max-buffers=1 drop=true "   
                 )
 
@@ -176,6 +177,7 @@ class ImageAnalyticsEngine:
                     f"tensor_converter ! "
                     f"tensor_transform mode=arithmetic option=typecast:float32,div:255.0 ! "
                     f"tensor_filter framework=tensorflow2-lite model={self.model_path} ! "
+                    f"tensor_decoder mode=bounding_boxes option1=blazeface ! "
                     f"appsink name=appsink emit-signals=true max-buffers=1 drop=true "   
                 )
         else:
@@ -228,6 +230,7 @@ class ImageAnalyticsEngine:
         if success:
             # res_array = np.frombuffer(map_info.data, dtype=np.float32)
             res_array = np.frombuffer(map_info.data, dtype=np.float32).copy()
+            log.info("Detection data: %s", res_array)
             
             if len(res_array) > 0:
                 ai_results = {"people": True, "fatigue": None, "raw": res_array}
@@ -279,12 +282,7 @@ class ImageAnalyticsEngine:
                 raw_data = result.get("raw")
                 is_people = result.get("people")
 
-                if is_people and raw_data is not None and len(raw_data) >= 4:
-                    log.info("--- [AI DATA] ---")
-                    log.info(f"Number of data: {len(raw_data)}")
-                    log.info(f"First 5 data: {raw_data[:5]}")
-                    log.info("-" * 30)
-
+                if is_people and raw_data is not None:
                     # Debug display
                     if self.debug_mode and self.overlay:
                         decoded = decode_blazeface(raw_data, width=self.width, height=self.height)
@@ -301,6 +299,11 @@ class ImageAnalyticsEngine:
                             self.overlay.set_property("data", svg_data)
                         else:
                             self.overlay.set_property("data", "")
+                    else:
+                        log.info("--- [AI DATA] ---")
+                        log.info(f"Number of data: {len(raw_data)}")
+                        log.info(f"First 5 data: {raw_data[:5]}")
+                        log.info("-" * 30)
                 else:
                     if self.debug_mode and self.overlay:
                         self.overlay.set_property("data", "")
@@ -310,31 +313,14 @@ def decode_blazeface(raw_data, score_threshold=0.75, width=640, height=480):
     """
     Decode the array 15,232 into face coordinates. (x, y, w, h)
     """
-    # Split the array: the first 14,336 numbers are Box, and the remaining 896 numbers are Score.
-    boxes = raw_data[:14336].reshape(896, 16)
-    scores = raw_data[14336:]
-    
-    sigmoid_scores = 1 / (1 + np.exp(-scores))
-    
-    best_idx = np.argmax(sigmoid_scores)
-    
-    if sigmoid_scores[best_idx] < score_threshold:
-        return None
-            
-    if best_idx == -1:
+    if len(raw_data) >= 4:
+        x = raw_data[0]
+        y = raw_data[1]
+        w = raw_data[2]
+        h = raw_data[3]
+    else:
         return None
 
-    raw_box = boxes[best_idx]
-    
-    # Scale to Pixel
-    cx = raw_box[1] / 128.0 * width
-    cy = raw_box[0] / 128.0 * height
-    w = raw_box[3] / 128.0 * width
-    h = raw_box[2] / 128.0 * height
-    
-    x = cx - w/2
-    y = cy - h/2
-    
     return x, y, w, h
 
 if __name__ == "__main__":
