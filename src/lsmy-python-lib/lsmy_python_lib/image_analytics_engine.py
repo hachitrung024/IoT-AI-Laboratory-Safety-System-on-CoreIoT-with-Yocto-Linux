21b7902480c77c1ad6a0aaf856ac1647b701134e
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-03-14 15:29:19 +0700

    Add log for debug segment fault in high  fps

--- src/lsmy-python-lib/lsmy_python_lib/image_analytics_engine.py
+++ src/lsmy-python-lib/lsmy_python_lib/image_analytics_engine.py
@@ -268,6 +268,7 @@ class ImageAnalyticsEngine:
         Convert sample -> numpy array and extract metadata.
         """
         # log.info("Received new sample from appsink")
+        log.info("DEBUG: Received new sample from appsink")
         sample = appsink.emit("pull-sample")
         if sample is None:
             return Gst.FlowReturn.OK
@@ -283,8 +284,10 @@ class ImageAnalyticsEngine:
             pipeline_latency = (current_pipeline_time - buf.pts) / Gst.MSECOND
 
         success, map_info = buf.map(Gst.MapFlags.READ)
+        log.info("DEBUG: Mapped buffer")
         if success:
             # res_array = np.frombuffer(map_info.data, dtype=np.float32)
+            log.info("DEBUG: Prepared copy data")
             res_array = np.frombuffer(map_info.data, dtype=np.float32).copy()
             # log.info("Detection data: %s", res_array)
             
@@ -295,6 +298,7 @@ class ImageAnalyticsEngine:
             
             self.measure_pipeline_metrics(self.inference_time, pipeline_latency)
             buf.unmap(map_info)
+            log.info("DEBUG: Unmapped buffer")
 
             if ai_results["people"]:
                 if self.result_queue.full():
@@ -303,6 +307,7 @@ class ImageAnalyticsEngine:
                     except:
                         pass
                 self.result_queue.put(ai_results)
+            log.info("DEBUG: Put result in queue")
 
         return Gst.FlowReturn.OK
     
@@ -420,12 +425,15 @@ class ImageAnalyticsEngine:
     def _main_loop(self):
         while not self._stop_event.is_set():
             result = None
+            log.info("DEBUG: Checking result queue")
             try:
                 result = self.result_queue.get(timeout=1)
             except queue.Empty:
                 pass
             except Exception as e:
                 log.warning("Error getting result from queue %s", e)
+
+            log.info("DEBUG: After Checking result queue")
 
             if self._stop_event.is_set():
                 break
